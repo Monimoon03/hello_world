8c21095861fd1566a4fdcc2f913f6b8d202aa944
usuario <usuario@MacBook-Air-de-usuario.local>
2021-11-16 13:54:56 -0600

    my first commit

---
+++ expermint.js
@@ -0,0 +1,8 @@
+const firstName = "Monica";
+const lastName = "Toro";
+
+const sentence = "Hola" = firstName + " " + lastName + "! Como estas?";
+const sentence_2 = `Hola ${firstName} + ${lastName} + ! Como estas?`;
+
+console.log(sentence);
+console.log(sentence2);
